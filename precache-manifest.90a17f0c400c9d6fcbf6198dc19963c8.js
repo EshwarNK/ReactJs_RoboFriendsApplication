86db5b25d9c0612c0d9a4f05fb441fd8d57904b0
EshwarNK <eshwarnk27@gmail.com>
2020-05-14 12:27:50 +0530

    Updates

---
+++ precache-manifest.90a17f0c400c9d6fcbf6198dc19963c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ce53b2897ad2644950",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/css/main.65fd427b.chunk.css"
+  },
+  {
+    "revision": "d1ce53b2897ad2644950",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/js/main.d1ce53b2.chunk.js"
+  },
+  {
+    "revision": "7a187f26461ea998d0ba",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/css/1.ac3e8b38.chunk.css"
+  },
+  {
+    "revision": "7a187f26461ea998d0ba",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/js/1.7a187f26.chunk.js"
+  },
+  {
+    "revision": "fc3cdd59ff55d253b6f9",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/js/runtime~main.fc3cdd59.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "db39ede94e989e6b3c39f3996ef2ed51",
+    "url": "/EshwarNK/ReactJs_RoboFriendsApplication/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/EshwarNK/ReactJs_RoboFriendsApplication/precache-manifest.90a17f0c400c9d6fcbf6198dc19963c8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/EshwarNK/ReactJs_RoboFriendsApplication/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
